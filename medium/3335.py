833e0755673fb4070ece63dc255dbf8a796e5097
GloamingBlue <1992491680@qq.com>
2025-05-14 00:35:07 +0800

    add medium 3335

--- medium/3335.py
+++ medium/3335.py
@@ -10,14 +10,14 @@ class Solution:
             cnt[ord(c) - 97] += 1
         m, n = t % 26, t // 26  # 得到轮数和需要单独处理的轮次，26次变换为一轮
 
-        for _  in range(n):
+        for _ in range(n):
             for i in range(26):  # 更新每轮字母的新个数
                 temp[i] = cnt[i] + cnt[i - 1]
             temp[1] += cnt[-1]
             cnt = [x % MOD for x in temp]
-            print(cnt)
 
-        return (sum(cnt) + sum(cnt[-m:])) % MOD
+        # 返回值需要特判情况，如果t是26的倍数sum(cnt[-m:])即为sum(cnt)，不应该再加上
+        return (sum(cnt) + sum(cnt[-m:])) % MOD if m > 0 else sum(cnt) % MOD
 
 
 if __name__ == '__main__':
